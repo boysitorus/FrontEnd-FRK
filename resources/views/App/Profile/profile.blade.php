a3b8b43f1245caac24a9af2bad453fa91193acab
boyysitorus <sitorusboy0123@gmail.com>
2024-02-22 08:45:44 +0700

    Profile

--- resources/views/App/Profile/profile.blade.php
+++ resources/views/App/Profile/profile.blade.php
@@ -2,8 +2,49 @@
 
 @section('content')
     <!-- ROLE -->
+
     <div class="mt-3 d-flex align-items-center justify-content-end">
         <span class="role">Peran Saat ini : Dosen Program Studi S1 Informatika</span>
     </div>
-    <!-- ROLE -->
+
+    <!-- END OF ROLE -->
+
+    {{-- NOTIFICATION --}}
+
+    <div class="notif w-100 mt-5 d-flex align-items-center justify-content-start ps-4 p-3">
+        <img class="me-4" src="{{ asset('assets/icon/Warn.svg') }}" alt="warn-icon">
+        <span>
+            <h5>
+                Menunggu Persetujuan dari Asesor
+            </h5>
+            <span class="d-block">
+                Rencana kerja yang anda susun sedang menunggu persetujuan dari asesor. Anda dapat mengedit rencana kerja
+                sebelum
+                disetujui oleh asesor. <br>
+                Silahkan hubungi asesor anda untuk meminta persetujuan asesor
+            </span>
+        </span>
+    </div>
+
+    {{-- END OF NOTIFICATION --}}
+
+    {{-- NAME --}}
+
+    <div class="bg-white w-100 mt-5 d-flex align-items-center justify-content-start ps-4 p-3">
+        <img src="" width="120" height="120" alt="">
+        <span class="ms-5">
+            <span class="">
+                Selamat Datang
+            </span>
+            <h3 style="font-weight: 700">
+                WILONA DIVA ARTHA SIMBOLON
+            </h3>
+            <span class="d-block">
+                Fakultas Informatika dan Teknik Elektro - Informatika
+            </span>
+        </span>
+    </div>
+
+    {{-- END OF NAME --}}
+
 @endsection
