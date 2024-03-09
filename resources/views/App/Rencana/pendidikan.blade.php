242eb1def7072fded47946eece713b09c6d61868
ifs21011-itdel <archicosemb@gmail.com>
2024-03-09 21:58:09 +0700

    feat element add J K modalJ modalK

--- resources/views/App/Rencana/pendidikan.blade.php
+++ resources/views/App/Rencana/pendidikan.blade.php
@@ -556,6 +556,145 @@
 </div>
 {{-- AKHIR BAGIAN I --}}
 
+{{-- BAGIAN J --}}
+<div id="pendidikan-J" class="card shadow-sm mt-5 ml-1 mr-1 bg-card">
+    <div class="card-body">
+        <h6><b>J. Koordinator Tugas  Akhir/Skripsi, Proyek Akhir  atau KP</b></h6>
+        <hr/>
+
+        <div class="row justify-content-end mr-0">
+            <button id="btnFrkPenelitianJ" type="button" class="btn btn-success col-md-auto m-1" data-bs-toggle="modal" data-bs-target="#modalPendidikan_J">
+                Tambah Kegiatan
+            </button>
+        </div>
+
+        <div class="text-sm">
+            <table id="tablePendidikan-J" class="table table-striped table-bordered mt-2 text-center align-middle" style="border: 2px;">
+                <thead>
+                    <tr>
+                        <th scope="col" rowspan="2" class="align-middle fw-bold">No.</th>
+                        <th scope="col" rowspan="2" class="align-middle fw-bold col-4">Kegiatan</th>
+                        <th scope="col" rowspan="2" class="align-middle fw-bold col-4">SKS Terhitung</th>
+                        <th scope="col" colspan="2 " class="allign-middle fw-bold col-2">Status</th>
+                        <th scope="col" rowspan="2" class="align-middle fw-bold col-2">Aksi</th>
+                    </tr>
+                    <tr>
+                        <th scope="col" class="fw-bold">Asesor 1</th>
+                        <th scope="col" class="fw-bold">Asesor 2</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td scope="row">1</td>
+                        <td class="text-start">Lorem ipsum dolor sit amet consectetur.</td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td>
+                        <button type="button" class="btn btn-secondary mr-1"><i class="bi bi-eyedropper"></i></button>
+                        <button type="button" class="btn btn-secondary"><i class="bi bi-trash3-fill"></i></i></button>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td scope="row">2</td>
+                        <td class="text-start">Lorem ipsum dolor sit amet consectetur. Semper gravida purus magna pellentesque mauris elit arcu pharetra.</td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td>
+                        <button type="button" class="btn btn-secondary mr-1"><i class="bi bi-eyedropper"></i></button>
+                        <button type="button" class="btn btn-secondary"><i class="bi bi-trash3-fill"></i></i></button>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td scope="row">3</td>
+                        <td class="text-start">Lorem ipsum dolor sit amet consectetur. Semper gravida purus magna pellentesque mauris elit arcu pharetra.</td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td>
+                        <button type="button" class="btn btn-secondary mr-1"><i class="bi bi-eyedropper"></i></button>
+                        <button type="button" class="btn btn-secondary"><i class="bi bi-trash3-fill"></i></i></button>
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    </div>
+</div>
+{{-- AKHIR BAGIAN J --}}
+
+{{-- BAGIAN K --}}
+<div id="pendidikan-K" class="card shadow-sm mt-5 ml-1 mr-1 bg-card">
+    <div class="card-body">
+        <h6><b>K. Asistensi tugas atau praktikum terhadap setiap kelompok.</b></h6>
+        <hr/>
+
+        <div class="row justify-content-end mr-0">
+            <button id="btnFrkPenelitianK" type="button" class="btn btn-success col-md-auto m-1" data-bs-toggle="modal" data-bs-target="#modalPendidikan_K">
+                Tambah Kegiatan
+            </button>
+        </div>
+
+        <div class="text-sm">
+            <table id="tablePendidikan-K" class="table table-striped table-bordered mt-2 text-center align-middle" style="border: 2px;">
+                <thead>
+                    <tr>
+                        <th scope="col" rowspan="2" class="align-middle fw-bold">No.</th>
+                        <th scope="col" rowspan="2" class="align-middle fw-bold col-3">Kegiatan</th>
+                        <th scope="col" rowspan="2" class="align-middle fw-bold">Jumlah Mahasiswa Bimbingan</th>
+                        <th scope="col" rowspan="2" class="align-middle fw-bold col-2">SKS Terhitung</th>
+                        <th scope="col" colspan="2 " class="allign-middle fw-bold col-2">Status</th>
+                        <th scope="col" rowspan="2" class="align-middle fw-bold col-2">Aksi</th>
+                    </tr>
+                    <tr>
+                        <th scope="col" class="fw-bold">Asesor 1</th>
+                        <th scope="col" class="fw-bold">Asesor 2</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td scope="row">1</td>
+                        <td class="text-start">Lorem ipsum dolor sit amet consectetur.</td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td>
+                        <button type="button" class="btn btn-secondary mr-1"><i class="bi bi-eyedropper"></i></button>
+                        <button type="button" class="btn btn-secondary"><i class="bi bi-trash3-fill"></i></i></button>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td scope="row">2</td>
+                        <td class="text-start">Lorem ipsum dolor sit amet consectetur. Semper gravida purus magna pellentesque mauris elit arcu pharetra.</td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td>
+                        <button type="button" class="btn btn-secondary mr-1"><i class="bi bi-eyedropper"></i></button>
+                        <button type="button" class="btn btn-secondary"><i class="bi bi-trash3-fill"></i></i></button>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td scope="row">3</td>
+                        <td class="text-start">Lorem ipsum dolor sit amet consectetur. Semper gravida purus magna pellentesque mauris elit arcu pharetra.</td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td>
+                        <button type="button" class="btn btn-secondary mr-1"><i class="bi bi-eyedropper"></i></button>
+                        <button type="button" class="btn btn-secondary"><i class="bi bi-trash3-fill"></i></i></button>
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    </div>
+</div>
+{{-- AKHIR BAGIAN K --}}
 
 {{-- TEMPAT MODAL TAMBAH KEGIATAN--}}
 
@@ -847,6 +986,62 @@
     </div>
 </div>
 {{-- AKHIR MODAL I --}}
+
+{{-- MULAI MODAL J --}}
+<div class="modal fade modal-lg" id="modalPendidikan_J" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog modal-dialog-centered" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h6 class="modal-title" id="exampleModalLabel">J. Koordinator Tugas Akhir/Skripsi, Proyek Akhir atau KP</h6>
+                <button class="btn-close" type="button" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+
+            <div class="modal-body">
+                <form>
+                    <div class="mb-3">
+                        <label for="nama" class="form-label">Nama Kegiatan</label>
+                        <input type="text" class="form-control" id="nama">
+                    </div>
+                </form>
+            </div>  
+
+            <div class="modal-footer">
+                <button type="submit" class="btn btn-primary">Simpan</button>
+            </div>
+        </div>
+    </div>
+</div>
+{{-- AKHIR MODAL J --}}
+
+{{-- MULAI MODAL K --}}
+<div class="modal fade modal-lg" id="modalPendidikan_K" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog modal-dialog-centered" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h6 class="modal-title" id="exampleModalLabel">K. Asistensi tugas atau praktikum terhadap setiap kelompok.</h6>
+                <button class="btn-close" type="button" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+
+            <div class="modal-body">
+                <form>
+                    <div class="mb-3">
+                        <label for="nama" class="form-label">Nama Kegiatan</label>
+                        <input type="text" class="form-control" id="nama">
+                    </div>
+                    <div class="mb-3">
+                        <label class="form-label">Jumlah Mhasiswa Bimbingan :</label>
+                        <input class="form-control" type="text">
+                    </div>
+                </form>
+            </div>  
+
+            <div class="modal-footer">
+                <button type="submit" class="btn btn-primary">Simpan</button>
+            </div>
+        </div>
+    </div>
+</div>
+{{-- AKHIR MODAL K --}}
 
 
 {{--TEMPAT MODAL EDIT --}}
