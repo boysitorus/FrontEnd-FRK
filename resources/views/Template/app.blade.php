8339270fcef525f23afa353c3c6363bfee006ae0
boyysitorus <sitorusboy0123@gmail.com>
2024-03-09 21:25:41 +0700

    sideNav

--- resources/views/Template/app.blade.php
+++ resources/views/Template/app.blade.php
@@ -19,15 +19,85 @@
         <nav id="sidebarMenu" class="collapse d-lg-block sidebar collapse bg-nav-utama">
             <div class="position-sticky">
                 <div class="list-group list-group-flush mx-3 mt-5">
-                    <a href="#" class="list-group-item bg-abu list-group-item-action py-2 ripple" aria-current="true">
-                        <i class="fas fa-tachometer-alt fa-fw me-3"></i><span>Profile</span>
+                    <a href="#" class="list-group-item bg-abu list-group-item-action py-2 ripple mt-5"
+                        aria-current="true">
+                        <i class="fas fa-tachometer-alt fa-fw me-3"></i><span>Profil</span>
                     </a>
-                    <a href="#" class="list-group-item list-group-item-action py-2 ripple active">
-                        <i class="fas fa-chart-area fa-fw me-3"></i><span>Rencana Kerja</span>
-                    </a>
+
+                    <div class="dropend">
+                        <a type="button" class="list-group-item list-group-item-action py-2 ripple active"
+                            data-bs-toggle="dropdown" aria-expanded="false">
+                            <i class="bi-caret-right-fill me-2"></i>Rencana Kerja
+                        </a>
+                        <ul class="dropdown-menu">
+                            <li><a href="#" class="list-group-item list-group-item-action py-2 ripple active">
+                                    <span>Rekap Kegiatan</span>
+                                </a></li>
+                        </ul>
+                    </div>
+
+                    <div class="dropend">
+                        <a type="button" class="list-group-item list-group-item-action py-2 ripple"
+                            data-bs-toggle="dropdown" aria-expanded="false">
+                            <i class="bi-caret-right-fill me-2"></i>Evaluasi Diri
+                        </a>
+                        <ul class="dropdown-menu">
+                            <li>
+                                <a href="#" class="list-group-item list-group-item-action py-2 ripple active">
+                                    <span>Rekap Kegiatan</span>
+                                </a>
+                            </li>
+                        </ul>
+                    </div>
+
+
+
                     <a href="#" class="list-group-item bg-abu list-group-item-action py-2 ripple"><i
                             class="fas fa-lock fa-fw me-3"></i><span>Evaluasi Diri</span></a>
-                    
+
+                    <a href="#" class="list-group-item bg-abu list-group-item-action py-2 ripple"><i
+                            class="fas fa-lock fa-fw me-3"></i><span>Riwayat Kegiatan</span></a>
+
+                    <div class="dropend">
+                        <a type="button" class="list-group-item list-group-item-action py-2 ripple"
+                            data-bs-toggle="dropdown" aria-expanded="false">
+                            <i class="bi-caret-right-fill me-2"></i>Asesor
+                        </a>
+                        <ul class="dropdown-menu">
+                            <li class="dropdown-submenu">
+                                <a type="button" class="list-group-item list-group-item-action py-2 ripple active"
+                                    data-bs-toggle="dropdown" aria-expanded="false">
+                                    <i class="bi-caret-right-fill me-2"></i>Evaluasi Diri
+                                </a>
+                                <ul class="dropdown-menu">
+                                    <li><a href="#" class="list-group-item list-group-item-action py-2 ripple">
+                                            <span>Rekap Kegiatan</span>
+                                        </a></li>
+                                    <li><a href="#" class="list-group-item list-group-item-action py-2 ripple">
+                                            <span>Rekap Kegiatan Disetujui</span>
+                                        </a></li>
+
+                                </ul>
+                            </li>
+
+                            <li class="dropdown-submenu">
+                                <a type="button" class="list-group-item list-group-item-action py-2 ripple active"
+                                    data-bs-toggle="dropdown" aria-expanded="false">
+                                    <i class="bi-caret-right-fill me-2"></i>Rencana Kerja
+                                </a>
+                                <ul class="dropdown-menu">
+                                    <li><a href="#" class="list-group-item list-group-item-action py-2 ripple">
+                                            <span>Rekap Kegiatan</span>
+                                        </a></li>
+                                    <li><a href="#" class="list-group-item list-group-item-action py-2 ripple">
+                                            <span>Rekap Kegiatan Disetujui</span>
+                                        </a></li>
+
+                                </ul>
+                            </li>
+                        </ul>
+                    </div>
+
                 </div>
             </div>
         </nav>
@@ -56,15 +126,16 @@
 
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <!-- Right elements -->
-                    <div class="ms-auto d-flex align-items-center justify-content-start">
+                    {{-- <div class="ms-auto d-flex align-items-center justify-content-start">
                         <img class="d-inline" src="{{ asset('assets/icon/Logout.svg') }}" alt="">
                         <a class="text-reset me-3 text-decoration-none" href="#">
                             <h5 class="ms-2 pt-2 " style="font-weight: 700;">Keluar</h5>
                         </a>
 
-                        <!-- Right elements -->
-                    </div>
-                    <!-- Collapsible wrapper -->
+                        
+                    </div> --}}
+                    <!-- Right elements -->
+
                 </div>
             </div>
             <!-- Container wrapper -->
@@ -81,8 +152,8 @@
     </main>
     <!--Main layout-->
 
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous">
     </script>
 </body>
 
