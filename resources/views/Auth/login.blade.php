ae9de3332cc835378f86a83c5099389444044770
boyysitorus <sitorusboy0123@gmail.com>
2024-03-14 13:02:10 +0700

    login page reduce

--- resources/views/Auth/login.blade.php
+++ resources/views/Auth/login.blade.php
@@ -41,10 +41,6 @@
                                             style="background-color: #FBFBFB; height: 58px" />
                                     </div>
 
-                                    <p class="small mb-2 pb-lg-2 d-flex align-items-start "><a
-                                            class="text-decoration-none" style="color: #696969" href="#!">Lupa kata
-                                            sandi?</a></p>
-
                                     <button
                                         style="background-color: #69839C; font-size: 2rem;font-weight: 600; width: 100%; height: 75px"
                                         class="btn text-white btn-lg px-5" type="submit">Masuk</button>
@@ -53,11 +49,7 @@
                             </div>
                         </div>
                     </div>
-                    <div class="d-flex justify-content-center">
-                        <p class="mt-3">Belum punya akun? <a href="#!" style="color: #3553BF"
-                                class="fw-bold text-decoration-none">Daftar Sekarang</a>
-                        </p>
-                    </div>
+                    
                 </div>
             </div>
         </div>
