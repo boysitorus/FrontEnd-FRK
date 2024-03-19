f417a60b38fbe9a57e3089db19927ad841af0303
Yefta Miranda <yeftamiranda@gmail.com>
2024-03-19 09:54:35 +0700

    menambahkan tabel i dan j

--- resources/views/App/Rencana/penelitian.blade.php
+++ resources/views/App/Rencana/penelitian.blade.php
@@ -291,4 +291,146 @@
                 </div>
             </div>
 
+            <div class="card shadow-sm mt-5 ml-1 mr-1 bg-card">
+                <div class="card-body">
+                    <h5><b>I. Sebagai asesor Beban Kerja Dosen dan Evaluasi Pelaksanaan Tridharma Perguruan Tinggi</b></h5>
+                    <hr/>
+
+                    <div class = "row justify-content-end mr-0">
+                        <button id="btnFrkPenelitianG" type="button" class="btn btn-success col-md-auto mt-2 mb-2">Tambah Kegiatan</button>
+
+                    </div>
+
+                    <table class="table table-striped table-bordered mt-2 text-center" style="border: 2px;">
+                        <thead>
+                            <tr>
+                                <th scope="col" rowspan="2" class="align-middle">No.</th>
+                                <th scope="col" rowspan="2" class="align-middle">Kegiatan</th>
+                                <th scope="col" rowspan="2" class="align-middle">Banyaknya BKD yang Terhitung</th>
+                                <th scope="col" rowspan="2" class="align-middle">SKS Terhitung</th>
+                                <th scope="col" colspan="2">Status</th>
+                                <th scope="col" rowspan="2" class="align-middle" style="width:100px;">Aksi</th>
+                            </tr>
+                            <tr>
+                                <th scope="col">Asesor 1</th>
+                                <th scope="col">Asesor 2</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td scope="row">1</td>
+                                <td>Lorem ipsum dolor sit amet consectetur. Semper gravida purus magna pellentesque mauris elit arcu pharetra.</td>
+                                <td></td>
+                                <td></td>
+                                <td></td>
+                                <td></td>
+                                <td>
+                                    <a href="#">
+                                        <img src="{{ asset('assets/icon/Vectoredit.svg') }}" alt="edit"/>
+                                    </a>
+                                    <a>
+                                        <img src="{{ asset('assets/icon/Vectordelete.svg') }}" alt="delete"/>
+                                    </a>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td scope="row">2</td>
+                                <td>Lorem ipsum dolor sit amet consectetur. Semper gravida purus magna pellentesque mauris elit arcu pharetra.</td>
+                                <td></td>
+                                <td></td>
+                                <td></td>
+                                <td></td>
+                                <td>
+                                    <a href="#">
+                                        <img src="{{ asset('assets/icon/Vectoredit.svg') }}" alt="edit"/>
+                                    </a>
+                                    <a>
+                                        <img src="{{ asset('assets/icon/Vectordelete.svg') }}" alt="delete"/>
+                                    </a>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td scope="row">3</td>
+                                <td>Lorem ipsum dolor sit amet consectetur. Semper gravida purus magna pellentesque mauris elit arcu pharetra.</td>
+                                <td></td>
+                                <td></td>
+                                <td></td>
+                                <td></td>
+                                <td>
+                                    <a href="#">
+                                        <img src="{{ asset('assets/icon/Vectoredit.svg') }}" alt="edit"/>
+                                    </a>
+                                    <a>
+                                        <img src="{{ asset('assets/icon/Vectordelete.svg') }}" alt="delete"/>
+                                    </a>
+                                </td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+
+            <div class="card shadow-sm mt-5 ml-1 mr-1 bg-card">
+                <div class="card-body">
+                    <h5><b>J. Menulis Jurnal Ilmiah</b></h5>
+                    <hr/>
+
+                    <div class = "row justify-content-end mr-0">
+                        <button id="btnFrkPenelitianG" type="button" class="btn btn-success col-md-auto mt-2 mb-2">Tambah Kegiatan</button>
+
+                    </div>
+
+                    <table class="table table-striped table-bordered mt-2 text-center" style="border: 2px;">
+                        <thead>
+                            <tr>
+                                <th scope="col" rowspan="2" class="align-middle">No.</th>
+                                <th scope="col" rowspan="2" class="align-middle">Kegiatan</th>
+                                <th scope="col" rowspan="2" class="align-middle">Kategori</th>
+                                <th scope="col" rowspan="2" class="align-middle">SKS Terhitung</th>
+                                <th scope="col" colspan="2">Status</th>
+                                <th scope="col" rowspan="2" class="align-middle" style="width:100px;">Aksi</th>
+                            </tr>
+                            <tr>
+                                <th scope="col">Asesor 1</th>
+                                <th scope="col">Asesor 2</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td scope="row">1</td>
+                                <td>Lorem ipsum dolor sit amet consectetur. Semper gravida purus magna pellentesque mauris elit arcu pharetra.</td>
+                                <td></td>
+                                <td></td>
+                                <td></td>
+                                <td></td>
+                                <td>
+                                    <a href="#">
+                                        <img src="{{ asset('assets/icon/Vectoredit.svg') }}" alt="edit"/>
+                                    </a>
+                                    <a>
+                                        <img src="{{ asset('assets/icon/Vectordelete.svg') }}" alt="delete"/>
+                                    </a>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td scope="row">2</td>
+                                <td>Lorem ipsum dolor sit amet consectetur. Semper gravida purus magna pellentesque mauris elit arcu pharetra.</td>
+                                <td></td>
+                                <td></td>
+                                <td></td>
+                                <td></td>
+                                <td>
+                                    <a href="#">
+                                        <img src="{{ asset('assets/icon/Vectoredit.svg') }}" alt="edit"/>
+                                    </a>
+                                    <a>
+                                        <img src="{{ asset('assets/icon/Vectordelete.svg') }}" alt="delete"/>
+                                    </a>
+                                </td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+
 @endsection
