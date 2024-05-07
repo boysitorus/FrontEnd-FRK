27e1b1cd951ad19e59d4289da483e353fd32a87d
Tabitha Aquila <ifs21049@students.del.ac.id>
2024-05-07 13:34:43 +0700

    perbaikan button x pada file

--- resources/views/App/Evaluasi/penelitian.blade.php
+++ resources/views/App/Evaluasi/penelitian.blade.php
@@ -1670,8 +1670,8 @@
 
                 // Tambahkan tombol hapus
                 var deleteBtn = document.createElement('button');
-                deleteBtn.classList.add('btn', 'btn-danger', 'btn-sm', 'btn-circle', 'ms-2');
-                deleteBtn.innerHTML = '<i class="bi bi-x"></i>';
+                deleteBtn.classList.add('btn', 'btn-close', 'btn-sm', 'ms-2');
+                // deleteBtn.innerHTML = '<i class="bi bi-x"></i>';
                 deleteBtn.addEventListener('click', (function(fileToRemove) {
                     return function() {
                         // Hapus file dari array file-file yang dipilih
